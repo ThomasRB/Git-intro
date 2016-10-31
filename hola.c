3cc19d0faed0d7718e3b7973757c147ae7970caa
Thomas Reeling Brouwer <ThomasRB@users.noreply.github.com>
2016-10-30 19:23:13 -0600

    Update hola.c

--- hola.c
+++ hola.c
@@ -1,16 +1,2 @@
-ola  q ace.
-Todo bien y vos
-<<<<<<< HEAD
-Estaba en clase de compu
-
-=======
-
-
-
-
-
-
-
-fsaklgbaslkbkvnab...
->>>>>>> 31e1f18b4fc9cae4f45e7b22669c258fe37af5e4
-ya pude lol :v jaja me siento bien jaquer :v
+.
+  
